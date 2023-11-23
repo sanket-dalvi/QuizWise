55220a24258a5a71c382b9a37876f364aaf1869f
Sanket Dalvi <dalvi.sanket26@gmail.com>
2023-11-22 23:16:33 -0500

    authentication and authorization implemented

--- QuizWise/forms.py
+++ QuizWise/forms.py
@@ -3,7 +3,20 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User  # Import your User model
 
-class CreateUserForm(UserCreationForm):
+class UserRegistrationForm(UserCreationForm):
+    ROLE_CHOICES = (
+        ('examinee', 'Examinee'),
+        ('examiner', 'Examiner'),
+    )
+
+    role = forms.ChoiceField(choices=ROLE_CHOICES, required=True)
+
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2', 'first_name', 'last_name', 'contact']
+        fields = ['email', 'username', 'password1', 'password2', 'first_name', 'last_name', 'contact', 'role']
+
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
--- QuizWise/views.py
+++ QuizWise/views.py
@@ -1,17 +1,59 @@
 # views.py
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserRegistrationForm, UserLoginForm
+from .auth_decorator  import examinee_required, examiner_required
+from django.contrib import messages
+
 
 def index(request):
-    return render(request, 'index.html') 
+    return render(request, "index.html")
 
-def create_user(request):
+def register_user(request):
     if request.method == 'POST':
-        form = CreateUserForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # Redirect to a success page or any other desired page
-            return redirect('success_page')  # Replace 'success_page' with the URL name
+            role = form.cleaned_data['role']
+            user = form.save(commit=False)
+            if role == 'examinee':
+                user.is_examinee = True
+            elif role == 'examiner':
+                user.is_examiner = True
+            user.save()
+            # Redirect to a success page
+            return render(request, 'success_page.html')  
     else:
-        form = CreateUserForm()
-    return render(request, 'create_user.html', {'form': form})
+        form = UserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            print(username)
+            print(password)
+            user = authenticate(request, username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                return render(request, 'success_page.html')  
+            else:
+                messages.error(request, 'Invalid username or password') 
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    # Redirect to a specific page after logout 
+    return redirect('login') 
+
+@examiner_required
+def examiner_test(request):
+    return render(request, 'success_page.html')
+
+
+def unauthorized(request):
+    return render(request, "unauthorized.html")
