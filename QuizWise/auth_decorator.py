2e520a51e1bcd4eebb2e68801855c0ba9b490d31
Sanket Dalvi <dalvi.sanket26@gmail.com>
2023-11-22 19:14:59 -0500

    moved css to html files

--- QuizWise/auth_decorator.py
+++ QuizWise/auth_decorator.py
@@ -18,7 +18,8 @@ def examinee_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.is_examinee:
-            # User is authenticated and is an Examinee
+            # User is authenticated and is an Examinee.0
+            .
             return view_func(request, *args, **kwargs)
         else:
             # Redirect or handle unauthorized access
