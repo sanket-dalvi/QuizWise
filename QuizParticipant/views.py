5e082a3ad448c854b61894146320a0f1d163273d
Alisha Bingewar <alishabingewar19@gmail.com>
2023-11-29 21:53:27 -0500

    Quiz participant UI changes

--- QuizParticipant/views.py
+++ QuizParticipant/views.py
@@ -4,3 +4,17 @@ from QuizWise.auth_decorator import examinee_required
 @examinee_required
 def home(request):
     return render(request, "QuizParticipant/home.html")
+
+@examinee_required
+def profile(request):
+    return render(request, "QuizParticipant/profile.html")
+
+
+@examinee_required
+def scores(request):
+    return render(request, "QuizParticipant/scores.html")
+
+
+@examinee_required
+def quiz_history(request):
+    return render(request, "QuizParticipant/quiz_history.html")
